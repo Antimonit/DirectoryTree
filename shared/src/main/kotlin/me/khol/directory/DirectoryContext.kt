dfb1e9fd892d0b6cddf7b8af71e5af9900343f34
David Khol <david@khol.me>
2022-05-20 22:02:22 +0900

    Add javadoc to DirectoryContext

--- shared/src/main/kotlin/me/khol/directory/DirectoryContext.kt
+++ shared/src/main/kotlin/me/khol/directory/DirectoryContext.kt
@@ -1,8 +1,26 @@
 package me.khol.directory
 
 class DirectoryContext(
+
+    /**
+     * Style of "pipes" displayed in the output.
+     */
     val separators: Separators = Separators(),
+
+    /**
+     * Sort files and directories by their names.
+     */
     val sortMode: SortMode = SortMode.NONE,
+
+    /**
+     * Reorder contents of all directories to put directories before files.
+     */
     val directoriesFirst: Boolean = false,
+
+    /**
+     * Strategy for merging directory and file names.
+     *
+     * See [CompactMode] for details.
+     */
     val compactMode: CompactMode = CompactMode.NONE,
 )
