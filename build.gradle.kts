5134daa19bc6a4e32dc2f2fd05778852fba4788a
David Khol <david@khol.me>
2022-05-20 14:10:28 +0900

    Extract kotlin version to root buildscript block

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 buildscript {
     repositories {
+        gradlePluginPortal()
         mavenCentral()
     }
     dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.19.0")
     }
 }
